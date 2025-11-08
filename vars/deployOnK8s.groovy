d96aed03d60847b7ebb51f6f6348ef70f42a9438
rowidarafiek <rowidarafiek83@gmail.com>
2025-11-08 19:33:35 +0200

    update deploy function

--- vars/deployOnK8s.groovy
+++ vars/deployOnK8s.groovy
@@ -1,9 +1,13 @@
-def callSimple(String imageName, String buildNumber) {
-    echo "Deploying to Kubernetes (simple)..."
+def call(String imageName, String buildNumber) {
+    echo "Deploying ${imageName}:${buildNumber} to Kubernetes..."
+
     sh """
-        kubectl set image deployment/myapp myapp=${imageName}:${buildNumber} || \
-        kubectl create deployment myapp --image=${imageName}:${buildNumber}
-        
-        kubectl expose deployment myapp --port=80 --target-port=8080 --type=LoadBalancer || true
+        if kubectl get deployment myapp; then
+            kubectl set image deployment/myapp myapp=${imageName}:${buildNumber}
+        else
+            kubectl create deployment myapp --image=${imageName}:${buildNumber}
+            kubectl expose deployment myapp --port=80 --target-port=8080 --type=LoadBalancer
+        fi
     """
 }
+
